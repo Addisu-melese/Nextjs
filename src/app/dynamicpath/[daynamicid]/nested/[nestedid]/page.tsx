3cfcaa038685307afc3b532fc5d62b24e9a7f937
Addisu <addis1melese@gmail.com>
2024-12-21 12:56:37 +0300

    adding error and some common page

--- src/app/dynamicpath/[daynamicid]/nested/[nestedid]/page.tsx
+++ src/app/dynamicpath/[daynamicid]/nested/[nestedid]/page.tsx
@@ -1,3 +1,9 @@
+import { error } from "console";
+import Error from "next/error";
+
+function getRandomInt(count:number) {
+  return Math.floor(Math.random()*count);
+}
 
 const page = ({params}:
     {
@@ -8,6 +14,11 @@ const page = ({params}:
         };
     }
 ) => {
+  const random=getRandomInt(2);
+  if(random===1)
+    {
+    throw new ErrorEvent("Error loading review")
+   }
   return (
     <div>daynamicid {params.daynamicid} nested {params.nestedid}</div>
   )
---
+++ src/app/error.tsx
@@ -0,0 +1,8 @@
+"user client"
+const ErrorBoundary = () => {
+  return (
+    <div>error ocured</div>
+  )
+}
+
+export default ErrorBoundary
